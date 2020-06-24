21ceceb427ba881252db31af78ce7284072efca5
IanKim <59094101+IanKim@users.noreply.github.com>
2020-06-23 22:36:30 -0400

    html:<header><h1>Todo list</h1></header>

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-'use strict';
-//  c58
+"use strict";
+
+//js refresh page every 8 seconds (완성 이후 지우기)
+
+setTimeout(function () {
+  window.location.reload(1);
+}, 8000);
+
+/* part3 */
+document.write("  1분코딩 JS기초 part3-2 event");
+console.log(" 1분코딩 JS기초 part3-2 event");
+
+//  c58 . js58
 //Make navbar trasnparent when it is on the top
