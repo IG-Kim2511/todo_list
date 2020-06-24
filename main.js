f6028373d2be30c68c28374f9d06dab967cf2d44
IanKim <59094101+IanKim@users.noreply.github.com>
2020-06-23 23:28:34 -0400

    css: form button:hover{

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ setTimeout(function () {
   window.location.reload(1);
 }, 8000);
 
-/* part3 */
-document.write("  1분코딩 JS기초 part3-2 event");
-console.log(" 1분코딩 JS기초 part3-2 event");
-
 //  c58 . js58
 //Make navbar trasnparent when it is on the top
+
+// Selectors
+
+// event listeners
+
+//function
